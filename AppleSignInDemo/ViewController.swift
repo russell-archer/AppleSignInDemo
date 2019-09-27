06c28ac40d2cac629ff26770100bcc25a05ec111
russell-archer <russell.archer@mac.com>
2019-09-27 18:29:32 +0100

    Initial Commit

---
+++ AppleSignInDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleSignInDemo
+//
+//  Created by Russell Archer on 27/09/2019.
+//  Copyright © 2019 Russell Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
